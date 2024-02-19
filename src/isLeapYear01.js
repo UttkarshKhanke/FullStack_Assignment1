e230687b0321498fe99d01434a7cfe00a7a73de0
UttkarshKhanke <uttkarsh.khanke@gmail.com>
2024-02-19 19:52:28 +0530

    requirement 1

---
+++ src/isLeapYear01.js
@@ -0,0 +1,4 @@
+import { isLeapYear } from './dateUtil.js';
+console.log(isLeapYear(2100)); 
+
+console.log(isLeapYear(2000));
---
+++ src/isLeapYear02.js
@@ -0,0 +1,4 @@
+import dateUtil from './dateUtil.js';
+
+console.log(dateUtil.isLeapYear(2100)); 
+console.log(dateUtil.isLeapYear(2000)); 
